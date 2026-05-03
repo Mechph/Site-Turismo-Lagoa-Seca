73415724085d662024a087ac7a4466531f516c2c
Pedro Henrique <138126897+Mechph@users.noreply.github.com>
2026-05-03 15:55:44 -0300

    Mudanças no Layot do Site

--- turismo-lagoa-seca/src/data/pontos.jsx
+++ turismo-lagoa-seca/src/data/pontos.jsx
@@ -1,21 +1,25 @@
+import ariticum from "../assets/images/AriticumChales.png";
+import convento from "../assets/images/CoventoIpuarana.jpg";
+import museu from "../assets/images/MuseudoIndio.png";
+
 const pontos = [
   {
     id: 1,
-    nome: "Centro de Lagoa Seca",
-    descricao: "Região central com comércio e cultura local.",
-    imagem: "https://images.unsplash.com/photo-1526772662000-3f88f10405ff"
+    nome: "Ariticum Chalés",
+    descricao: "Localizado na zona rural de Lagoa Seca, Paraíba, o Ariticum Chalés apresenta-se como uma opção de hospedagem focada em proporcionar tranquilidade e um contato mais próximo com a natureza.",
+    imagem: ariticum
   },
   {
     id: 2,
-    nome: "Zona Rural",
-    descricao: "Paisagens naturais e clima agradável.",
-    imagem: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
+    nome: "Convento Ipuarana",
+    descricao: "O convento é um ponto de referência espiritual e cultural, especialmente durante a tradicional Festa de Santo Antônio, e também funciona como casa de retiros, encontros e hospedagem..",
+    imagem: convento
   },
   {
     id: 3,
-    nome: "Feira Livre",
-    descricao: "Cultura local e produtos regionais.",
-    imagem: "https://images.unsplash.com/photo-1519681393784-d120267933ba"
+    nome: "Museu do Indio",
+    descricao: "Fazendo parte do Covento Ipuarana O Museu do Indio traz um deslumbre das antigas culturas que peregrinaram pelo estado da Paraíba.",
+    imagem: museu
   }
 ];
 
--- turismo-lagoa-seca/src/pages/Home.jsx
+++ turismo-lagoa-seca/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import Navbar from "../components/Navbar";
 import "./home.css";
+import bg from "../assets/images/EuAmoLagoaSeca.jpeg";
 
 export default function Home() {
   return (
     <>
       <Navbar />
 
-      <section className="hero">
+      <section 
+        className="hero"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
         <h1>Descubra Lagoa Seca</h1>
         <p>Natureza, cultura e tranquilidade no coração da Paraíba</p>
       </section>
